56b72436511bcf78513344ce46ab0035b4dcc98f
Volshebnick12 <j_a_shurik@bk.ru>
2022-02-14 09:10:35 +0300

    fix problem

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const screens = document.querySelectorAll('.screen');
 const startBn = document.querySelector('#start');
 const canvas = document.getElementById('life');
 const ctx = canvas.getContext('2d');
+const width = 100;
+const height = 100;
 let mas=[];
 let count = 0;
 let timer;
-
-let time = 0
 
 // Переключение экраннов
 startBn.addEventListener('click', (event) => {
@@ -19,82 +19,104 @@ startBn.addEventListener('click', (event) => {
 canvas.onclick = function(event) {
   let x = event.offsetX;
   let y = event.offsetY;
-  console.log(x);
-  console.log(y);
-  x = Math.floor(x/5); // 500 / 5 = 100  
-  y = Math.floor(y/5); // 500 / 5 = 100  
+  x = Math.floor(x / 5); // 500 / 5 = 100  
+  y = Math.floor(y / 5); // 500 / 5 = 100  
   mas [y][x] = 1;
-  console.log(mas);
   drawField();
-}
+};
+
+goLife();
 
 function goLife () {
-  const n=100, m=100;
-  for (let i=0; i<m; i++) {
+  mas = [];
+  for (let i=0; i < width; i++) {
     mas [i] = [];
-    for (let j=0; j<n; j++) {
+    for (let j=0; j < height; j++) {
       mas [i][j] = 0;
     }  
   }
 }
-goLife();
 
 // Отрисовываем точки
 function drawField () {
-  ctx.clearRect (0, 0, 500, 500);
-  for (let i=0; i<100; i++) {
-    for (let j=0; j<100; j++) {
+  ctx.clearRect (0, 0, width * 5, height * 5);
+  for (let i=0; i < width; i++) {
+    for (let j=0; j < height; j++) {
         if (mas[i][j]==1) {
           ctx.fillStyle = '#35aeff';
-          ctx.fillRect(j*5, i*5, 5, 5);
+          ctx.fillRect(j * 5, i * 5, 5, 5);
         }
       }
    }
 }
 
 // Модерация  жизни
-function startLife () {
-  let mas2 = [];
-  for (let i=0; i<100; i++) {
-    mas2[i]=[];
-    for (let j=0; j<100; j++) {
-      let  neighbors = 0;
-      let result = false;
-      if (mas[fpm(i)-1][j]==1) neighbors++; // проверка соседей сверху
-      if (mas[i][fpp(j)+1]==1) neighbors++; // проверка соседей справа
-      if (mas[fpp(i)+1][j]==1) neighbors++; // проверка соседей снизу
-      if (mas[i][fpm(j)-1]==1) neighbors++; // проверка соседей слева
-      if (mas[fpm(i)-1][fpp(j)+1]==1) neighbors++; // проверка соседей справа сверху
-      if (mas[fpp(i)+1][fpp(j)+1]==1) neighbors++; // проверка соседей справа снизу
-      if (mas[fpp(i)+1][fpm(j)-1]==1) neighbors++; // проверка соседей справа снизу
-      if (mas[fpm(i)-1][fpm(j)-1]==1) neighbors++; // проверка соседей справа снизу
-      (neighbors==2 || neighbors==3) ? mas2[i][j]=1 : mas2[i][j]=0;
+function getAliveNeighbors(x, y, field) {
+  let prevX = x - 1;
+  if (x === 0) {
+    prevX = width - 1;
+  }
+  let nextX = x + 1;
+  if (x === width - 1) {
+    nextX = 0;
+  }
+  let prevY = y - 1;
+    if (y === 0) {
+      prevY = height - 1;
+  }
+  let nextY  = y + 1;
+    if (y ===  height - 1) {
+      nextY = 0
+    }
+  if (!field[prevX] || !field[nextX]) {
+    console.error('ERROR', x, y, field);
+    return 0;
+  }
+  let neighbors = 0;
+  if (field[prevX][y]) neighbors++; // проверка слева
+  if (field[nextX][y]) neighbors++; // проверка справа
+  if (field[x][prevY]) neighbors++; // проверка сверху
+  if (field[x][nextY]) neighbors++; // проверка снизу
+  if (field[prevX][prevY]) neighbors++; // проверка слева сверху
+  if (field[nextX][prevY]) neighbors++; // проверка справа сверху
+  if (field[prevX][nextY]) neighbors++; // проверка слева снизу
+  if (field[nextX][nextY]) neighbors++; // проверка справа снизу
+  return  neighbors;
+}
+
+// проверка условий игры
+function runner() {
+  const mas2 = [];
+  // console.log(mas);
+  for (let i = 0; i < width; i++) {
+    mas2[i] = [];
+    for (let j = 0; j < height; j++) {
+      // console.log('i, j', i, j);
+      const neighbors = getAliveNeighbors(i, j, mas);
+      if (neighbors === 3) {
+        mas2[i][j] = 1;
+      } else if (mas[i][j] === 1 && neighbors === 2) {
+        mas2[i][j] = 1;
+      } else {
+        mas2[i][j] = 0;
+      }
     }
   }
   mas = mas2;
   drawField();
   count++;
   document.getElementById('count').innerHTML = count;
-  timer = setTimeout(startLife, 300);
+}
+
+function startLife() {
+  timer = setInterval(runner, 100);
   startGame.disabled = true;
 }
 
-// Выход за пределы поля
-function fpm (i) {
-  if(i==0) return 100;
-  else return i;
-}
-
-// Выход за пределы поля
-function fpp (i) {
-  if(i==99) return -1;
-  else return i;
-}
-
 document.getElementById('startGame').onclick = startLife;
-document.getElementById('endGame').onclick = endtLife;
+document.getElementById('endGame').onclick = endLife;
 
-function endtLife () {
-  mas = 0;
+function endLife() {
+  clearInterval(timer);
   startGame.disabled = false;
 }
